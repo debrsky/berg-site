3c9da9feb712e5ff687eb9f76e9061e7e7ea2cbd
debrsky <debrsky@gmail.com>
2021-08-01 12:04:14 +1000

    add catching errors in gulp validate

--- gulp/validate-html.js
+++ gulp/validate-html.js
@@ -1,10 +1,12 @@
 const gulp = require(`gulp`);
+const plumber = require(`gulp-plumber`);
 const htmlValidator = require("gulp-w3c-html-validator");
 const bemValidator = require("gulp-html-bem-validator");
 
 module.exports = function validateHTML() {
   return gulp
     .src(`public/**/*.html`)
+    .pipe(plumber()) // catch "service unavailable" of gulp-w3c-html-validator
     .pipe(htmlValidator())
     .pipe(bemValidator());
 };
