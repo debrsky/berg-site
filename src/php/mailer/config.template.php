c87d01b8cf46332a350d396abee0c78986e9b608
debrsky <debrsky@gmail.com>
2021-08-05 07:57:34 +1000

    add config.template.php

---
+++ src/php/mailer/config.template.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array(
+  'Host' => 'smtp.yandex.ru', // SMTP сервер вашей почты
+  'Username' => 'your_login', // Логин на почте
+  'Password' => 'password', // Пароль на почте
+  'SMTPSecure' => 'ssl',
+  'Port' => 465,
+  'FromEmail' => 'mail@yandex.ru',
+  'FromName' => 'Имя отправителя',
+
+  'Reciever' => 'reciever@yandex.ru' // Получатель письма
+);
