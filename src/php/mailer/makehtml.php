3b8bcde048a33f80d5173661dc0b8f31531e53a0
debrsky <debrsky@gmail.com>
2021-08-05 15:14:04 +1000

    edit email markup for ms outlook

--- src/php/mailer/makehtml.php
+++ src/php/mailer/makehtml.php
@@ -10,26 +10,26 @@ function makehtml($data) {
   $cargo = <<<END
   <table border="1" cellpadding="2" cellspacing="0" style="margin:0; padding:0; border-collapse: collapse; width: 100%;">
     <caption>
-      Груз <span style="font-weight: bold; font-size: large;">{$_($data->cargo->name)}</span>
+      Груз <span style="font-weight: bold; ">{$_($data->cargo->name)}</span>
     </caption>
     <thead>
       <tr>
-        <td>Вес, кг</td>
-        <td>Объем, м³</td>
-        <td>Количество мест</td>
-        <td>Температурный режим</td>
-        <td>Объявленная стоимость, руб.</td>
-        <td>Страховка</td>
+        <td style="text-align: center; min-width: 12ch;">Вес, кг</td>
+        <td style="text-align: center; min-width: 12ch;">Объем, м³</td>
+        <td style="text-align: center; min-width: 12ch;">Количество<br>мест</td>
+        <td style="text-align: center;">Температурный<br>режим</td>
+        <td style="text-align: center;">Объявленная<br>стоимость, руб.</td>
+        <td style="text-align: center;">Страховка</td>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->weight)}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->volume)}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->qty)}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->condition)}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->value)}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->cargo->insurance)}</td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->weight)}</span></td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->volume)}</span></td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->qty)}</span></td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->condition)}</span></td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->value)}</span></td>
+        <td style="text-align: center;"><span style="font-weight: bold;">{$_($data->cargo->insurance)}</span></td>
       </tr>
     </tbody>
   </table>
@@ -47,32 +47,32 @@ function makehtml($data) {
   <table border="1" cellpadding="2" cellspacing="0" style="margin:0; padding:0; border-collapse: collapse; width: 100%;">
     <caption>
       {$_(array('consigner'=>'Грузоотправитель', 'consignee'=>'Грузополучатель', 'payer'=>'Плательщик')[$role])}
-      <span style="font-weight: bold; font-size: large;">{$_($data->{$role}->name)}</span>
+      <span style="font-weight: bold;">{$_($data->{$role}->name)}</span>
     </caption>
 
     <tbody>
       <tr>
-        <td>ИНН</td>
-        <td>КПП</td>
-        <td>ОГРН</td>
-        <td>Юридический адрес</td>
+        <td style="padding: 2px;">ИНН</td>
+        <td style="padding: 2px;">КПП</td>
+        <td style="padding: 2px;">ОГРН</td>
+        <td style="padding: 2px;">Юридический адрес</td>
       </tr>
       <tr>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$role}->{'INN'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$role}->{'KPP'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$role}->{'OGRN'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$role}->{'address'})}</td>
+        <td style="padding: 2px;">{$_($data->{$role}->{'INN'})}</td>
+        <td style="padding: 2px;">{$_($data->{$role}->{'KPP'})}</td>
+        <td style="padding: 2px;">{$_($data->{$role}->{'OGRN'})}</td>
+        <td style="padding: 2px;">{$_($data->{$role}->{'address'})}</td>
       </tr>
       {$_(
           ($role == 'payer')
             ? ''
-            : '<tr><td colspan="4">Контактное лицо по вопросам передачи груза <span style="font-weight: bold; font-size: large;">'
+            : '<tr><td colspan="4">Контактное лицо по вопросам передачи груза <span style="font-weight: bold; ">'
             . $data->{$role}->{'cargoContactFio'} . ' ' . $data->{$role}->{'cargoContactTel'} . '</span></td></tr>'
       )}
       </tr>
       {$_(
         ($data->{$role}->{'isPayer'})
-          ? '<tr><td colspan="4">Контактное лицо по вопросам оплаты <span style="font-weight: bold; font-size: large;">'
+          ? '<tr><td colspan="4">Контактное лицо по вопросам оплаты <span style="font-weight: bold; ">'
           . $data->{$role}->{'payingContactFio'} . ' ' . $data->{$role}->{'payingContactTel'} . '</span></td></tr>'
           : ''
       )}
@@ -86,7 +86,7 @@ function makehtml($data) {
   <table border="1" cellpadding="2" cellspacing="0" style="margin:0; padding:0; border-collapse: collapse; width: 100%;">
     <caption>
       {$_(array('consigner'=>'Грузоотправитель', 'consignee'=>'Грузополучатель', 'payer'=>'Плательщик')[$role])}
-      <span style="font-weight: bold; font-size: large;">
+      <span style="font-weight: bold; ">
         {$_($data->{$role}->{'name'})} {$_($data->{$role}->{'tel'})}
       </span>
     </caption>
@@ -115,11 +115,11 @@ function makehtml($data) {
     </thead>
     <tbody>
       <tr>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$operation}->{'date'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$operation}->{'time'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$operation}->{'place'})}</td>
-        <td style="font-weight: bold; font-size: large;">{$_(array("$operation-point-terminal" => 'Терминал', "$operation-point-client" => 'Подача машины', "$operation-point-pickup" => 'Передача на трассе')[$data->{$operation}->{'point'}])}</td>
-        <td style="font-weight: bold; font-size: large;">{$_($data->{$operation}->{'pointClientAddress'})}</td>
+        <td style="font-weight: bold; ">{$_($data->{$operation}->{'date'})}</td>
+        <td style="font-weight: bold; ">{$_($data->{$operation}->{'time'})}</td>
+        <td style="font-weight: bold; ">{$_($data->{$operation}->{'place'})}</td>
+        <td style="font-weight: bold; ">{$_(array("$operation-point-terminal" => 'Терминал', "$operation-point-client" => 'Подача машины', "$operation-point-pickup" => 'Передача на трассе')[$data->{$operation}->{'point'}])}</td>
+        <td style="font-weight: bold; ">{$_($data->{$operation}->{'pointClientAddress'})}</td>
       </tr>
     </tbody>
   </table>
@@ -130,54 +130,57 @@ function makehtml($data) {
 
   $html = <<<END
   <div class="email" style="max-width: 190mm; font-size: 12pt;">
-    <div style="font-size: large; border-bottom: 1px solid black;">
-      <div style="float: left;">Заявка на грузоперевозку</div>
-      <div style="float: right;">{$_($data->meta->timestamp)}</div>
-      <div style="clear: both;"></div>
-    </div>
+    <table border="0" cellpadding="0" cellspacing="0" style="margin:0; padding:0; width: 100%;">
+      <tbody>
+        <tr>
+          <td>Заявка на грузоперевозку</td>
+          <td style="text-align: right;">{$_($data->meta->timestamp)}</td>
+        </tr>
+      </tbody>
+    </table>
 
     <div style="padding: 1em 0;">
-      Дата отправления: <span style="font-size: large; font-weight: bold;">{$_($order_date->format('Y-m-d'))}</span>
-      Направление: <span style="font-size: large; font-weight: bold;">$direction</span>
+      Дата отправления: <span style=" font-weight: bold; font-size: large;">{$_($order_date->format('Y-m-d'))}</span>
+      Направление: <span style=" font-weight: bold; font-size: large;">$direction</span>
     </div>
 
+    <div>&nbsp;</div>
     {$_($counterAgent->{'consigner'})}
+
+    <div>&nbsp;</div>
     {$_($counterAgent->{'consignee'})}
 
+    <div>&nbsp;</div>
     {$_($cargo)}
 
+    <div>&nbsp;</div>
     <table border="1" cellpadding="2" cellspacing="0" style="margin:0; padding:0; border-collapse: collapse; width: 100%; margin-top: 0.5em;">
       <thead>
         <tr>
           <td></td>
-          <td style="width: 50%;">Сдача груза</td>
-          <td style="width: 50%;">Получение груза</td>
+          <td style="width: 50%;">Сдача груза <span style="font-weight: bold;">{$_($data->{'loading'}->{'place'})}</span></td>
+          <td style="width: 50%;">Получение груза <span style="font-weight: bold;">{$_($data->{'unloading'}->{'place'})}</span></td>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>Дата</td>
-          <td style="font-weight: bold; font-size: large;">{$_($data->{'loading'}->{'date'})}</td>
+          <td style="font-weight: bold; ">{$_($data->{'loading'}->{'date'})}</td>
           <td></td>
         </tr>
         <tr>
           <td>Время</td>
-          <td style="font-weight: bold; font-size: large;">{$_($data->{'loading'}->{'time'})}</td>
+          <td style="font-weight: bold; ">{$_($data->{'loading'}->{'time'})}</td>
           <td></td>
         </tr>
         <tr>
-          <td>Город</td>
-          <td style="font-weight: bold; font-size: large;">{$_($data->{'loading'}->{'place'})}</td>
-          <td style="font-weight: bold; font-size: large;">{$_($data->{'unloading'}->{'place'})}</td>
-        </tr>
-        <tr>
           <td>Способ</td>
-          <td style="font-weight: bold; font-size: large;">{$_(array(
+          <td style="font-weight: bold; ">{$_(array(
             'loading-point-terminal' => 'терминал',
             'loading-point-client' => 'подача машины',
             'loading-point-pickup' => 'забрать груз на трассе')[$data->{'loading'}->{'point'}])}
           </td>
-          <td style="font-weight: bold; font-size: large;">{$_(array(
+          <td style="font-weight: bold; ">{$_(array(
             'unloading-point-terminal' => 'терминал',
             'unloading-point-client' => 'подача машины',
             'unloading-point-pickup' => 'передать груз на трассе')[$data->{'unloading'}->{'point'}])}
@@ -185,14 +188,14 @@ function makehtml($data) {
         </tr>
         <tr>
           <td>Адрес</td>
-          <td style="font-weight: bold; font-size: large;">
+          <td style="font-weight: bold; ">
             {$_(
               ($data->{'loading'}->{'point'} == 'loading-point-terminal')
                 ? ''
                 : $data->{'loading'}->{'pointClientAddress'}
             )}
           </td>
-          <td style="font-weight: bold; font-size: large;">
+          <td style="font-weight: bold; ">
             {$_(
               $data->{'unloading'}->{'point'} == 'unloading-point-terminal'
                 ? ''
@@ -202,15 +205,18 @@ function makehtml($data) {
         </tr>
       </tbody>
     </table>
+
+    <div>&nbsp;</div>
     {$_($counterAgent->{'payer'})}
-    <p>Форма оплаты: <span style="font-weight: bold; font-size: large;">{$_($data->{'paymentForm'})}</span></p>
-    <p>Дополнительная информация: <span style="font-weight: bold; font-size: large;">{$_($data->{'memo'})}</span></p>
+
+    <p>Форма оплаты: <span style="font-weight: bold; ">{$_($data->{'paymentForm'})}</span></p>
+    <p>Дополнительная информация: <span style="font-weight: bold; ">{$_($data->{'memo'})}</span></p>
     <p>Заявку составил:
-      <span style="font-weight: bold; font-size: large;">{$_($data->{'author'}->{'fio'})}</span>,
+      <span style="font-weight: bold; ">{$_($data->{'author'}->{'fio'})}</span>,
       тел
-      <span style="font-weight: bold; font-size: large;">{$_($data->{'author'}->{'tel'})}</span>,
+      <span style="font-weight: bold; ">{$_($data->{'author'}->{'tel'})}</span>,
       email
-      <span style="font-weight: bold; font-size: large;">{$_($data->{'author'}->{'email'})}</span></p>
+      <span style="font-weight: bold; ">{$_($data->{'author'}->{'email'})}</span></p>
   </div>
   END;
 
