e792822e529d99ea14895cd50d2743ac042a7af5
debrsky <debrsky@gmail.com>
2021-08-08 14:30:28 +1000

    add user place detection

---
+++ src/js/common/set-place.js
@@ -0,0 +1,54 @@
+import {suggestionToken} from "../config";
+
+const url =
+  "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address";
+
+const options = {
+  method: "GET",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: "Token " + suggestionToken
+  }
+};
+
+export const setPlace = async () => {
+  const userPlace = localStorage.getItem("userPlace");
+  if (userPlace) return userPlace;
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) return;
+    const place = await response.json();
+    const {city, region} = place.location.data;
+
+    const placeRegions = {
+      Приморский: "Владивосток", // eslint-disable-line sonarjs/no-duplicate-string
+      Хабаровский: "Хабаровск",
+      Амурская: "Благовещенск"
+    };
+
+    const placeCities = [
+      "Владивосток",
+      "Уссурийск",
+      "Хабаровск",
+      "Благовещенск"
+    ];
+
+    let userPlaceDetected;
+    if (placeCities.includes(city)) {
+      userPlaceDetected = city;
+    } else if (Object.keys(placeRegions).includes(region)) {
+      userPlaceDetected = region;
+    } else {
+      userPlaceDetected = "Владивосток";
+    }
+
+    localStorage.setItem("userPlace", userPlaceDetected);
+
+    return userPlaceDetected;
+  } catch (err) {
+    console.error(err);
+  }
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
-console.log("It works!");
+import {setPlace} from "./common/set-place.js";
+
+(async () => {
+  const userPlace = await setPlace();
+  select.value = userPlace;
+  select.dispatchEvent(new Event("change"));
+})();
+
+const select = document.getElementById("contacts__place-selector");
+
+select.addEventListener("change", (event) => {
+  const target = event.target;
+  const place = target.value;
+  localStorage.setItem("userPlace", place);
+
+  target.style.width = `${place.length}ch`;
+
+  const contactsElement = document.getElementById("contacts");
+  const placeDependentElements = contactsElement.querySelectorAll(
+    "[data-place]"
+  );
+  placeDependentElements.forEach((element) => {
+    element.hidden = element.dataset.place !== place;
+  });
+});
