056bc9ce329981b32815c7a7e67d796325baa8d0
debrsky <debrsky@gmail.com>
2022-02-07 17:02:36 +1000

    use new version of gulp-w3c-html-validator

--- gulp/validate-html.js
+++ gulp/validate-html.js
@@ -1,12 +1,13 @@
 const gulp = require(`gulp`);
 const plumber = require(`gulp-plumber`);
-const htmlValidator = require("gulp-w3c-html-validator");
 const bemValidator = require("gulp-html-bem-validator");
 
-module.exports = function validateHTML() {
+module.exports = async function validateHTML() {
+  const {htmlValidator} = await import("gulp-w3c-html-validator");
   return gulp
     .src(`public/**/*.html`)
     .pipe(plumber()) // catch "service unavailable" of gulp-w3c-html-validator
-    .pipe(htmlValidator())
+    .pipe(htmlValidator.analyzer())
+    .pipe(htmlValidator.reporter())
     .pipe(bemValidator());
 };
