dcc1462cdb6b06f692ee2655b5caef12030752e5
debrsky <debrsky@gmail.com>
2022-02-12 22:32:11 +1000

    disable doiuse

--- gulp/less.js
+++ gulp/less.js
@@ -15,25 +15,22 @@ export default function less() {
     .pipe(lessG({math: "parens-division"}))
     .pipe(
       postcss([
-        autoprefixer(),
-        doiuse({
-          onFeatureUsage: function (info) {
-            const selector = info.usage.parent.selector;
-            const property = `${info.usage.prop}: ${info.usage.value}`;
-
-            let status = info.featureData.caniuseData.status.toUpperCase();
-
-            if (info.featureData.missing) {
-              status = "NOT SUPPORTED".red;
-            } else if (info.featureData.partial) {
-              status = "PARTIAL SUPPORT".yellow;
-            }
-
-            console.log(
-              `\n${status}:\n\n    ${selector} {\n        ${property};\n    }\n`
-            );
-          }
-        }) /*, csso() */
+        autoprefixer()
+        // doiuse({
+        //   onFeatureUsage: function (info) {
+        //     const selector = info.usage.parent.selector;
+        //     const property = `${info.usage.prop}: ${info.usage.value}`;
+        //     let status = info.featureData.caniuseData.status.toUpperCase();
+        //     if (info.featureData.missing) {
+        //       status = "NOT SUPPORTED";
+        //     } else if (info.featureData.partial) {
+        //       status = "PARTIAL SUPPORT";
+        //     }
+        //     console.log(
+        //       `\n${status}\n${info.message}\n:\n\n    ${selector} {\n        ${property};\n    }\n`
+        //     );
+        //   }
+        // }) /*, csso() */
       ])
     )
     .pipe(rename(`style.css`))
