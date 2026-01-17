be08bd5d333022d45b3b579d7e4b4cd087c67173
debrsky <debrsky@gmail.com>
2026-01-17 14:55:48 +1000

    fix(client-doc): increase price precision in upd generator
    Update price_without_nds formatting to use 6 decimal places instead of 4 for better accuracy.

--- src/js/client-doc/upd.js
+++ src/js/client-doc/upd.js
@@ -57,7 +57,7 @@ export function generateUPD(data, options = {}) {
     <td class="upd-text-center">${item?.mUcode ?? ''}</td>
     <td class="upd-text-center">${item?.mU ?? ''}</td>
     <td class="upd-text-center">${fmtNumber(item?.qty ?? '')}</td>
-    <td class="upd-money">${fmtMoney(item?.price_without_nds ?? 0, 4)}</td>
+    <td class="upd-money">${fmtMoney(item?.price_without_nds ?? 0, 6)}</td>
     <td class="upd-money">${fmtMoney(item?.amount_without_nds ?? 0)}</td>
     <td class="upd-text-center">--</td>
     <td class="upd-text-center">${itemNds === 0 ? '--' : `${itemNds}%`}</td>
